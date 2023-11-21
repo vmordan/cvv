0dd93c503ae8f3f9d593143e001e6d9ba4ca3742
Mordan Vitalii <mordan@ispras.ru>
2023-11-21 14:19:31 +0700

    Allow to redirect to another host:port in comparison data

--- utils/bin/get-compare-data.py
+++ utils/bin/get-compare-data.py
@@ -23,6 +23,7 @@ parser = get_args_parser('Upload comparison data for 2 last children of a given 
 parser.add_argument('job',
                     help='Node job identifier or its name (note, it must have at least 2 children for comparison)')
 parser.add_argument('-o', '--out', help='File name, in which comparison data json will be saved')
+parser.add_argument('--redirect-host', dest="redirect", help="Redirect urls to another host:port")
 args = parser.parse_args()
 
 with Session(args) as session:
@@ -31,15 +32,20 @@ with Session(args) as session:
     new_traces_without_marks = int(data['new_traces']) - int(data['new_traces_marked'])
     missing_traces_without_marks = int(data['missing_trace']) - int(data['missing_trace_marked'])
     all_traces_without_marks = int(data['unsafes']) - int(data['unsafes_marked'])
+    if args.redirect:
+        host = args.redirect
+    else:
+        host = args.host
+
     mes = f"### Statistics\n" \
           f" - New issues: {data['new_traces']}, without review: {new_traces_without_marks} " \
-          f"({args.host}/reports/component/{data['root_report']}/unsafes/?cmp={data['compared_job_id']})\n" \
+          f"({host}/reports/component/{data['root_report']}/unsafes/?cmp={data['compared_job_id']})\n" \
           f" - Missing issues: {data['missing_trace']}, without review: {missing_traces_without_marks} " \
-          f"({args.host}/reports/component/{data['compared_root_report']}/unsafes/?cmp={data['job_id']})\n" \
+          f"({host}/reports/component/{data['compared_root_report']}/unsafes/?cmp={data['job_id']})\n" \
           f" - Total issues: {data['unsafes']}, without review: {all_traces_without_marks} " \
-          f"({args.host}/reports/component/{data['root_report']}/unsafes/)\n\n" \
+          f"({host}/reports/component/{data['root_report']}/unsafes/)\n\n" \
           f"### Links\n" \
-          f" - Link to the new results: {args.host}/jobs/{data['job_id']}\n" \
-          f" - Link to the previous results: {args.host}/jobs/{data['compared_job_id']}\n" \
-          f" - Link to the comparison with previous results: {args.host}/reports/comparison/{job_id_or_name}"
+          f" - Link to the new results: {host}/jobs/{data['job_id']}\n" \
+          f" - Link to the previous results: {host}/jobs/{data['compared_job_id']}\n" \
+          f" - Link to the comparison with previous results: {host}/reports/comparison/{job_id_or_name}"
     print(mes)
